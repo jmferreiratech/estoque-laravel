75162ccce2c2299b98604ca1b25069da7569e3d9
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-06-02 15:41:22 -0300

    Changes namespace to 'estoque'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Exceptions;
+namespace estoque\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace estoque\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use estoque\User;
+use estoque\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
