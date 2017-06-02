2bb283404827d6bbd93427dd4490c089dc434185
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-06-02 16:22:31 -0300

    Creates, populates and accesses database. '.env' file is changed accordingly

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,18 @@
 
 namespace estoque\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class ProdutoController extends Controller {
 
     public function lista() {
-        return '<h1>Listagem de produtos com Laravel</h1>';
+        $html = '<h1>Listagem de produtos com Laravel</h1>';
+        $html .= '<ul>';
+        $produtos = DB::select('select * from produtos');
+        foreach ($produtos as $p) {
+            $html .= '<li> Nome: ' . $p->nome . ', Descrição: ' . $p->descricao . '</li>';
+        }
+        $html .= '</ul>';
+        return $html;
     }
 }
