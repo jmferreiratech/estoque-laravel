222d7760b882aedd0341fc0661578d5d1ea36f97
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-06-02 16:00:54 -0300

    Adds a product controller and a route to it

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace estoque\Http\Controllers;
+
+class ProdutoController extends Controller {
+
+    public function lista() {
+        return '<h1>Listagem de produtos com Laravel</h1>';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 Route::get('/', function () {
     return '<h1>Primeira lógica com Laravel</h1>';
 });
+
+Route::get('/produtos', 'ProdutoController@lista');
