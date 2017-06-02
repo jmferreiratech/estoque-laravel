76091b41d89d49bdcd3c927ec7064eea3a7798e5
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-06-02 17:00:08 -0300

    Adds product model, migration and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call('ProdutoTableSeeder');
+    }
+}
+
+class ProdutoTableSeeder extends Seeder {
+
+    public function run() {
+        DB::insert(
+            'insert into produtos (nome, quantidade, valor, descricao) values (?,?,?,?)',
+            array('Geladeira', 2, 5900.00, 'Side by Side com gelo na porta'));
+        DB::insert(
+            'insert into produtos (nome, quantidade, valor, descricao) values (?,?,?,?)',
+            array('Fogão', 5, 950.00, 'Painel automático e forno elétrico'));
+        DB::insert('insert into produtos (nome, quantidade, valor, descricao) values (?,?,?,?)',
+            array('Microondas', 1, 1520.00, 'Manda SMS quando termina de esquentar'));
     }
 }
