2b9b4d346bb5d38f6640abcedaaf27bbef4a5827
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-06-05 11:02:56 -0300

    Adds view details page

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -10,4 +10,12 @@ class ProdutoController extends Controller {
         $produtos = DB::select('select * from produtos');
         return view('listagem')->with('produtos', $produtos);
     }
+
+    public function mostra($id) {
+        $resposta = DB::select('select * from produtos where id = ?', [$id]);
+        if (empty($resposta)) {
+            return "Esse produto não existe";
+        }
+        return view('detalhes')->with('p', $resposta[0]);
+    }
 }
