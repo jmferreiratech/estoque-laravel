c56d1561bd0bbc72c04e9d2cfb65e42bdf638550
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-06-05 15:00:16 -0300

    Extracts out blade template and applies some style to principal

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -8,7 +8,7 @@ class ProdutoController extends Controller {
 
     public function lista() {
         $produtos = DB::select('select * from produtos');
-        return view('listagem')->with('produtos', $produtos);
+        return view('produto.listagem')->with('produtos', $produtos);
     }
 
     public function mostra($id) {
@@ -16,6 +16,6 @@ class ProdutoController extends Controller {
         if (empty($resposta)) {
             return "Esse produto não existe";
         }
-        return view('detalhes')->with('p', $resposta[0]);
+        return view('produto.detalhes')->with('p', $resposta[0]);
     }
 }
