ec8c6c931c334e95b3fdfcc823068ad97ebee707
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-06-05 09:18:20 -0300

    Adds some style to the presentation

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -1,15 +1,21 @@
 <html>
-    <body>
+<head>
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Controle de estoque</title>
+</head>
+<body>
+    <div class="container">
         <h1>Listagem de produtos</h1>
-        <table>
+        <table class="table table-striped table-bordered table-hover">
             <?php foreach ($produtos as $p): ?>
-            <tr>
-                <td><?= $p->nome ?></td>
-                <td><?= $p->valor ?></td>
-                <td><?= $p->descricao ?></td>
-                <td><?= $p->quantidade ?></td>
-            </tr>
+                <tr>
+                    <td><?= $p->nome ?></td>
+                    <td><?= $p->valor ?></td>
+                    <td><?= $p->descricao ?></td>
+                    <td><?= $p->quantidade ?></td>
+                </tr>
             <?php endforeach ?>
         </table>
-    </body>
+    </div>
+</body>
 </html>
